ee33540d9e8c4550ab86684bb838377a17cb6504
Katrina-19 <72166776+Katrina-19@users.noreply.github.com>
2021-11-29 01:28:43 +0300

    Добавьте файлы проекта.

---
+++ regChecking/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace regChecking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbReg = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(24, 61);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(313, 278);
+            this.tbText.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите строку";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Регулярное выражение";
+            // 
+            // tbReg
+            // 
+            this.tbReg.Location = new System.Drawing.Point(24, 396);
+            this.tbReg.Name = "tbReg";
+            this.tbReg.Size = new System.Drawing.Size(313, 22);
+            this.tbReg.TabIndex = 7;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(465, 61);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(308, 278);
+            this.tbResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbReg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbReg;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ regChecking/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace regChecking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbResult.Clear();
+            string text = tbText.Text;
+            string reg = tbReg.Text;
+            char[] arr = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                arr[i] = text[i];
+            }
+
+            switch (reg)
+            {
+                case "[A-Z]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsUpper(arr[i]))
+                        {
+
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[a-z]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsLower(arr[i]))
+                        {
+
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[A-Za-z]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsLetter(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[A-Za-z0-9]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsLetterOrDigit(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[A-Za-z0-9_]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsLetterOrDigit(arr[i]) || arr[i].ToString() == "_")
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case @"\w":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsLetterOrDigit(arr[i]) || arr[i].ToString() == "_")
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[^A-Za-z0-9_]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (!Char.IsLetterOrDigit(arr[i]) || arr[i].ToString() != "_")
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case @"\W":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (!Char.IsLetterOrDigit(arr[i]) || arr[i].ToString() != "_")
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[0-9]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsDigit(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case @"\d":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsDigit(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[^0-9]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (!Char.IsDigit(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case @"\D":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (!Char.IsDigit(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[0-9A-Fa-f]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsNumber(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+
+                }
+                case "[.,!?:;...]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsPunctuation(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[\t]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsWhiteSpace(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[\t\n\r\f\v]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsSeparator(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case @"\s":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsSeparator(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[^\t\n\r\f\v]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (!Char.IsSeparator(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case @"\S":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (!Char.IsSeparator(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "[\x00-\x1F\x7F]":
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (Char.IsControl(arr[i]))
+                        {
+                            tbResult.ForeColor = Color.Red;
+                            tbResult.Text += arr[i].ToString();
+                        }
+                    }
+
+                    break;
+                }
+                case "":
+                {
+                    MessageBox.Show(
+                        "Вы ничего не ввели",
+                        "Предупреждение",
+                        MessageBoxButtons.OK);
+                    break;
+                }
+                default:
+                {
+                    MessageBox.Show(
+                        "Вы ввели не регулярное выражение",
+                        "Предупреждение",
+                        MessageBoxButtons.OK);
+                    break;
+                }
+            }
+            if (tbResult.Text == "")
+            {
+                MessageBox.Show(
+                    "Строка не содержит регулярное выражение",
+                    "Предупреждение",
+                    MessageBoxButtons.OK);
+            }
+        }
+    }
+}
